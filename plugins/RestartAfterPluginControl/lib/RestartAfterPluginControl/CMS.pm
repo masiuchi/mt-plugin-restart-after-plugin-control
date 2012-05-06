6357c88e6cf1d06266160f3f93afc195a8c89a0c
masiuchi <masahiro.iuchi@gmail.com>
2012-05-06 11:20:25 +0900

    modified for not running using normal CGI

--- plugins/RestartAfterPluginControl/lib/RestartAfterPluginControl/CMS.pm
+++ plugins/RestartAfterPluginControl/lib/RestartAfterPluginControl/CMS.pm
@@ -8,11 +8,12 @@ use MT::CMS::Plugin;
 sub plugin_control {
     my ( $app ) = @_;
 
-    MT::Touch->touch( 0, 'config' );
-
     MT::CMS::Plugin::plugin_control( $app );
 
-    $app->{redirect_use_meta} = 1;
+    if ( $ENV{FAST_CGI} ) {
+        MT::Touch->touch( 0, 'config' );
+        $app->{redirect_use_meta} = 1;
+    }
 }
 
 1;
