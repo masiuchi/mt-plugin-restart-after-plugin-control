66a7deb46740e37c280808c1bace9307aba73033
masiuchi <masahiro.iuchi@gmail.com>
2012-05-07 20:28:57 +0900

    changed overwritten way of plugin_control method

--- plugins/RestartAfterPluginControl/RestartAfterPluginControl.pl
+++ plugins/RestartAfterPluginControl/RestartAfterPluginControl.pl
@@ -3,34 +3,41 @@ use strict;
 use warnings;
 use base 'MT::Plugin';
 
-our $VERSION = '0.02';
-our $NAME    = ( split /::/, __PACKAGE__ )[-1];
+use MT::Touch;
+use MT::CMS::Plugin;
 
-my $plugin = __PACKAGE__->new({
-    name     => $NAME,
-    id       => lc $NAME,
-    key      => lc $NAME,
-    version  => $VERSION,
-    author_name => 'masiuchi',
-    author_link => 'https://github.com/masiuchi',
-    plugin_link => 'https://github.com/masiuchi/mt-plugin-restart-after-plugin-control',
-    description => 'Restart FastCGI process after changing plugin status.',
-});
-MT->add_plugin( $plugin );
+our $VERSION = '0.03';
+our $NAME = ( split /::/, __PACKAGE__ )[-1];
 
-sub init_registry {
-    my ( $p ) = @_;
+my $plugin = __PACKAGE__->new(
+    {   name        => $NAME,
+        id          => lc $NAME,
+        key         => lc $NAME,
+        version     => $VERSION,
+        author_name => 'masiuchi',
+        author_link => 'https://github.com/masiuchi',
+        plugin_link =>
+            'https://github.com/masiuchi/mt-plugin-restart-after-plugin-control',
+        description =>
+            'Restart FastCGI process after changing plugin status.',
+    }
+);
+MT->add_plugin($plugin);
 
-    my $pkg = '$' . $NAME . '::' . $NAME;
-    $p->registry({
-        applications => {
-            cms => {
-                methods => {
-                    plugin_control => $pkg . '::CMS::plugin_control',
-                },
-            },
-        },
-    });
+{
+    my $orig = \&MT::CMS::Plugin::plugin_control;
+
+    no warnings 'redefine';
+    *MT::CMS::Plugin::plugin_control = sub {
+        my ($app) = @_;
+
+        $orig->($app);
+
+        if ( $ENV{FAST_CGI} && !$app->{_errstr} && $app->{redirect} ) {
+            MT::Touch->touch( 0, 'config' );
+            $app->{redirect_use_meta} = 1;
+        }
+    };
 }
 
 1;
