79bab2d63696bb8f5e0168c656fdfb7adf611b1a
masiuchi <masahiro.iuchi@gmail.com>
2012-05-08 06:57:41 +0900

    added redirect process

--- plugins/RestartAfterPluginControl/RestartAfterPluginControl.pl
+++ plugins/RestartAfterPluginControl/RestartAfterPluginControl.pl
@@ -4,6 +4,7 @@ use warnings;
 use base 'MT::Plugin';
 
 use MT::Touch;
+use MT::Util;
 use MT::CMS::Plugin;
 
 our $VERSION = '0.03';
@@ -20,6 +21,11 @@ my $plugin = __PACKAGE__->new(
             'https://github.com/masiuchi/mt-plugin-restart-after-plugin-control',
         description =>
             'Restart FastCGI process after changing plugin status.',
+        registry => {
+            applications =>
+                { cms => { methods => { do_nothing => sub {}, }, }, },
+        },
+        init_request => \&_init_req,
     }
 );
 MT->add_plugin($plugin);
@@ -38,6 +44,26 @@ MT->add_plugin($plugin);
             $app->{redirect_use_meta} = 1;
         }
     };
+}
+
+sub _init_req {
+    my ($app) = @_;
+
+    if ( my $touched = MT::Touch->latest_touch( 0, 'config' ) ) {
+        $touched = MT::Util::ts2epoch( undef, $touched, 1 );
+        my $startup = $app->{fcgi_startup_time };
+        if ( $startup && $touched > $startup ) {
+            my $mode = $app->param( '__mode' );
+            $app->param( '__mode', 'do_nothing' );
+            $app->redirect(
+                $app->uri(
+                    mode => $mode,
+                    args => $app->{parameters},
+                ),
+                UseMeta => 1,
+            );
+        }
+    }
 }
 
 1;
