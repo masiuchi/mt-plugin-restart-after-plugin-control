cf3a3d61c91ab33c7ce796f60d33f325e0264344
masiuchi <masahiro.iuchi@gmail.com>
2012-05-09 07:56:36 +0900

    modified for not using http referer, and tidied

--- plugins/RestartAfterPluginControl/RestartAfterPluginControl.pl
+++ plugins/RestartAfterPluginControl/RestartAfterPluginControl.pl
@@ -22,8 +22,9 @@ my $plugin = __PACKAGE__->new(
         description =>
             'Restart FastCGI process after changing plugin status.',
         registry => {
-            applications =>
-                { cms => { methods => { redirect_meta => \&_redirect_meta, }, }, },
+            applications => {
+                cms => { methods => { redirect_meta => \&_redirect_meta, }, },
+            },
         },
         init_request => \&_init_request,
     }
@@ -40,40 +41,34 @@ MT->add_plugin($plugin);
 
         if ( $ENV{FAST_CGI} && !$app->{_errstr} && $app->{redirect} ) {
             MT::Touch::NoCache->touch( 0, 'config' );
-            return _redirect_meta( $app );
+            return _redirect_meta($app);
         }
     };
 }
 
 sub _redirect_meta {
-    my ( $app ) = @_;
+    my ($app) = @_;
 
     my $redirect = $app->{redirect};
     $app->{redirect} = '';
-    return '<meta http-equiv="refresh" content="0.1;url='
-        . $redirect
-        . '">';
+    return '<meta http-equiv="refresh" content="0.1;url=' . $redirect . '">';
 }
 
 sub _init_request {
     my ($app) = @_;
 
     my $touched = MT::Touch::NoCache->latest_touch( 0, 'config' );
-    if ( $touched ) {
+    if ($touched) {
         $touched = MT::Util::ts2epoch( undef, $touched, 1 );
-        my $startup = $app->{fcgi_startup_time };
+        my $startup = $app->{fcgi_startup_time};
         if ( $startup && $touched > $startup ) {
-            my $mode = $app->param( '__mode' );
+            my $mode = $app->param('__mode');
             $app->param( '__mode', 'redirect_meta' );
-            my $redirect_uri = $ENV{HTTP_REFERER};
-            my $app_uri      = $app->uri();
-            if ( $redirect_uri !~ m/${app_uri}/ ) {
-                $redirect_uri = $app->uri(
-                    mode => $mode,
-                    args => $app->{parameters},
-                );
-            }
-            $app->redirect( $redirect_uri );
+            my $redirect_uri = $app->uri(
+                mode => $mode,
+                args => $app->{parameters},
+            );
+            $app->redirect($redirect_uri);
         }
     }
 }
